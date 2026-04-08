7253d469b8207fb79a2e86c520cf950c07ac6513
bao-nguyenngoc2 <bao.nguyenngoc2@vib.com.vn>
2026-04-08 20:32:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -91,3 +91,10 @@ function calculate() {
 
    return result1 + result2;
 }
+
+function calculate1() {
+   var result1 = sum(2, 3);
+   var result2 = multiply(4, 5);
+
+   return result1 + result2;
+}
